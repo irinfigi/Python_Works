77789cf0dedb2bd663a33b535f6802737219d7e4
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-14 19:04:34 +0530

    new file:   character.py

---
+++ character.py
@@ -0,0 +1,11 @@
+ch=input("Enter character in VIBGYOR:")
+if ch=='v' or ch=='V':
+    print("Violet")
+elif ch=='i' or ch=='I':
+    print("Indigo")
+elif ch=='b' or ch=='B':
+    print("Blue")
+elif ch=='g' or ch=='G':
+    print("Green")
+else:
+    print("Invalid")
