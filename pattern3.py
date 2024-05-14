4796fd8eebd2bf4f03ad46cf2fdca51cd6ccad12
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-15 01:07:54 +0530

    new file:   pattern.py 	new file:   pattern2.py 	new file:   pattern3.py

---
+++ pattern3.py
@@ -0,0 +1,13 @@
+i=1
+limit=5
+while i<=limit:
+    k=1
+    while k<=(limit-i):
+        print(" ",end='')
+        k=k+1
+    j=1
+    while j<=i:
+        print("a",end=' ')
+        j=j+1
+    print()
+    i=i+1
