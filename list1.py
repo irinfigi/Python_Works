97b2dc10ff395822aacfebe39a8b4bac3f92eecb
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-15 08:53:44 +0530

    new file:   list1.py

---
+++ list1.py
@@ -0,0 +1,11 @@
+age_list=[22,23,12,34,56,67,87,24]
+#print(len(age_list))
+#age_list.append(102)
+
+#print(age_list[0])
+#age_list.insert(1,55)
+#age_list.pop(1)
+age_list.remove(87)
+print(age_list)
+# age_list.pop(1)
+# print(age_list)
