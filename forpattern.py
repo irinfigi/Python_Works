5eac255be984e54e590aeb4901a547a7b64b2805
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-17 22:22:14 +0530

    new file:   forpattern.py 	new file:   slicing.py

---
+++ forpattern.py
@@ -0,0 +1,12 @@
+# limit=5
+# for i in range(limit):
+#     for j in range(limit-i):
+#         print(" ",end='')
+#     for k in range(i):
+#         print("*",end=' ')
+#     print()
+limit=5
+for i in range(limit):
+    for j in range(limit):
+        print("*",end='')
+    print()
