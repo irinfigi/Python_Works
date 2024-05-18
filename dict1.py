7573fbf14dc54442bd1c15b0e0652bb5556373f0
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-19 02:37:13 +0530

    new file:   dict1.py 	new file:   tuple.py

---
+++ dict1.py
@@ -0,0 +1,6 @@
+emp_salary={'irin':100,'juval':200,'andrea':300,'tina':400}
+print(emp_salary)
+print(emp_salary['irin'])
+emp_salary['john']=500
+emp_salary.__setitem__('figi',600)
+print(emp_salary)
---
+++ tuple.py
@@ -0,0 +1,8 @@
+mytuple=(2,3,5,6,7,'irin',9.3)
+for i in mytuple:
+    print(i)
+# i=0
+# size=len(mytuple)
+# while i<size:
+#     print(mytuple[i])
+#     i=i+1
