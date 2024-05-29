984957d59cddc3b2472c01f7d147f14ddd6bfb8e
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-29 21:50:40 +0530

    new file:   module_concept/vowels.py

---
+++ module_concept/vowels.py
@@ -0,0 +1,7 @@
+s1=input("Enter the string:")
+vowels=0
+for i in s1:
+    if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+        vowels=vowels+1
+print("The number of vovwels are:",vowels)
+#print(vowels)
