7ad0cf32303fe9cd79a40a03cc895932d325b9ed
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-29 15:11:44 +0530

    new file:   module_concept/__pycache__/circle.cpython-312.pyc 	new file:   module_concept/circle.py 	new file:   module_concept/sample.py

---
+++ module_concept/circle.py
@@ -0,0 +1,5 @@
+pi=3.14
+def find_area(r):
+    return pi*r*r
+def find_per(r):
+    return 2*pi*r
---
+++ module_concept/sample.py
@@ -0,0 +1,5 @@
+from circle import find_area,find_per
+radius=int(input("enter radius"))
+area=find_area(radius)
+peri=find_per(radius)
+print("Area:",area,"perimeter:",peri)
