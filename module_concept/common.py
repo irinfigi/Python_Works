99c86c9fe9f1f6993ec20f68e5cc577b6dc267e7
Irin F Mampilly <irinfigi123@gmail.com>
2024-05-29 21:29:57 +0530

    new file:   module_concept/common.py 	modified:   module_concept/self.py

---
+++ module_concept/common.py
@@ -0,0 +1,5 @@
+s1=input("Enter first string:")
+s2=input("Enter first string:")
+a=set(s1)&set(s2)
+for i in a:
+    print(i)
--- module_concept/self.py
+++ module_concept/self.py
@@ -1,16 +1,15 @@
 class Employee:
     company='ABCD'
-    location='Thrissur'
+    loc='Thrissur'
     def __init__(self,id,name,salary):
-        self.empid=id
-        self.empname=name
-        self.empsalary=salary
-    def getdetails(self):
-        print(self.empid,self.empname,self.empsalary)
-obj1=Employee('emp1','irin',30000)
-obj2=Employee('emp2','andrea',20000)
-obj3=Employee('emp3','tina',40000)
-obj1.getdetails()
-obj2.getdetails()
-obj3.getdetails()
-    
+        self.emp_id=id
+        self.emp_name=name
+        self.emp_salary=salary
+    def get_details(self):
+        print(self.emp_id,self.emp_name,self.emp_salary)
+obj1=Employee('emp1','irin',23000)
+obj2=Employee('emp2','andrea',22000)
+obj3=Employee('emp3','tina',25000)
+obj1.get_details()
+obj2.get_details()
+obj3.get_details()
